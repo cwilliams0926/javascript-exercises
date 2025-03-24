7303e7820fc0fa88f9ead3d48b18e5a895f4fc19
cwilliams0926 <coopwilliams03@hotmail.com>
2025-03-24 11:29:52 -0400

    Complete test 1

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let temp = "";
+    for(let i = 0; i < num; i++) {
+        temp = temp + string;
+    }
+    return temp;
 };
 
 // Do not edit below this line
