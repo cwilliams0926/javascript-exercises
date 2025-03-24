7b82c8398fc0cc548796a579dfff87a0421f1832
cwilliams0926 <coopwilliams03@hotmail.com>
2025-03-24 12:02:03 -0400

    Complete all tests

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,11 +1,9 @@
 const reverseString = function(string) {
-    // create array and make string into array
     const stringArray = [];
     for(let i = 0; i < string.length; i++) {
         stringArray[i] = string.charAt(string.length - i - 1);
     }
-    // reverse the array
-    // return string of all the indeces concatenated
+    
     let reversedString = "";
     for(let i = 0; i < stringArray.length; i++) {
         reversedString += stringArray[i];
