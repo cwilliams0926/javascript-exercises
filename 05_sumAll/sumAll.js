92dd33451f6d15b8002f28a1495b7184c3dbd096
cwilliams0926 <coopwilliams03@hotmail.com>
2025-03-25 10:30:48 -0400

    Complete all tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,16 @@
 const sumAll = function(num1, num2) {
+    // Error cases
+    if(num1 < 0 || num2 < 0) return "ERROR";
+    if(!Number.isInteger(num1) || !Number.isInteger(num2)) return "ERROR";
+    
+    // If first number is larger
+    if(num1 > num2) {
+        let temp = num2;
+        num2 = num1;
+        num1 = temp;
+    }
+
+    // Summing the numbers
     let sum = 0;
     for(let i = num1; i <= num2; i++) {
         sum += i;
