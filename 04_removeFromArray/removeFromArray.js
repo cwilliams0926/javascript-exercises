73d538a831c57c76cd65684bb46458674567f386
cwilliams0926 <coopwilliams03@hotmail.com>
2025-03-25 09:08:15 -0400

    Complete test 1

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,7 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, argument) {
+    let index = array.indexOf(argument);
+    array.splice(index, 1);
+    return array;
 };
 
 // Do not edit below this line
