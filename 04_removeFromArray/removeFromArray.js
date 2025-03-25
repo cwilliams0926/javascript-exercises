7cf176168cf17006e6ed0cc849414b59fd9d22bd
cwilliams0926 <coopwilliams03@hotmail.com>
2025-03-25 09:59:40 -0400

    Complete all tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,9 +1,14 @@
 const removeFromArray = function(array, ...args) {
     let index;
     for(let i = 0; i < args.length; i++) {
-        index = array.indexOf(args[i]);
-        if(array[index] === args[i]) {
+        let currentArg = args[i];
+        index = array.indexOf(currentArg);
+        // Ensure that the types match
+        if(array[index] === currentArg) {
             array.splice(index, 1);
+            // Decrement i so that we look for other instances of
+            // the currentArg in the array
+            i--;
         }
     }
     return array;
