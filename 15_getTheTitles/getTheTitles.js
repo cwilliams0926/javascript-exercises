ec06705494d76bcd7744c3eace688876a5a28f96
cwilliams0926 <coopwilliams03@hotmail.com>
2025-11-04 11:06:06 -0500

    Complete test 15

--- 15_getTheTitles/getTheTitles.js
+++ 15_getTheTitles/getTheTitles.js
@@ -1,5 +1,8 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(arr) {
+    return arr.reduce((titleArr, obj) => {
+        titleArr.push(obj.title);
+        return titleArr;
+    }, []);
 };
 
 // Do not edit below this line
