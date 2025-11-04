9630cb9ef7c345cefa8ef1a590100502ad83f029
cwilliams0926 <coopwilliams03@hotmail.com>
2025-11-04 10:24:59 -0500

    Complete test 13

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    str = str
+        .toLowerCase()
+        .trim()
+        .replaceAll(" ", "")
+        .replaceAll("!", "")
+        .replaceAll(".", "")
+        .replaceAll(",", "");
+    const reverseStr = str.split("").reverse().join("");
+    return str === reverseStr;
 };
 
 // Do not edit below this line
