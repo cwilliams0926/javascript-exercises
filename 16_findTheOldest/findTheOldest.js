da410520cfa95c5ead7da8bbcecc9ae3d187e986
cwilliams0926 <coopwilliams03@hotmail.com>
2025-11-05 10:23:28 -0500

    Complete test 16

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    return arr.reduce((obj, value) => {
+        if(value.yearOfDeath === undefined) {
+            value.yearOfDeath = new Date().getFullYear();
+        }
 
+        value.age = value.yearOfDeath - value.yearOfBirth;
+        if(obj.age === undefined) return value;
+        if(value.age > obj.age) return value;
+        return obj;
+    }, {});
 };
 
 // Do not edit below this line
